663ddd4eccb9ffd22fa0cad07eb9c9fed3d5ff71
Bishant Nayak <bishant.nayak44@gmail.com>
2023-07-14 00:44:17 +0530

    Static paths updated

--- streaming_backend/urls.py
+++ streaming_backend/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from vdo_api.routers.userRouters import router as userRouter
 from vdo_api.routers.videoRouters import videoRouter
@@ -9,3 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(videoRouter.urls)),
 ]
+
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
