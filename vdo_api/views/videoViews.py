93c8a16f166d54a6342d718d1e2cf406c9a0e8cb
Bishant Nayak <bishant.nayak44@gmail.com>
2023-07-14 14:32:25 +0530

    ser clss

--- vdo_api/views/videoViews.py
+++ vdo_api/views/videoViews.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
 from rest_framework import permissions
-from vdo_api.serializers.videoSerializer import VideoReadOnlyHyperlinkedSerializer
+from vdo_api.serializers.videoSerializer import VideoReadOnlySerializer
 from streaming_backend.utils import getModel
 
 class VideoViewSet(viewsets.ModelViewSet):
     model = getModel("vdo_api.Video")
     queryset = model.objects.all().order_by('-created_at')
-    serializer_class = VideoReadOnlyHyperlinkedSerializer
+    serializer_class = VideoReadOnlySerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
